748ae2abd3ce350999d899b453c99d8907227754
Pavan <101943293+Pavana004@users.noreply.github.com>
2022-11-02 18:20:24 +0530

    change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const mongoose = require("mongoose");
-const port = 5000 || process.env.PORT;
+const port = 5000||process.env.PORT;
 const userinfo = require("./router");
 
 
