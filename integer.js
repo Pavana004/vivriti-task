f2d6f1a621b5a8a593bc67b0aed5d2ecae12a59d
Pavan <101943293+Pavana004@users.noreply.github.com>
2022-11-19 11:27:06 +0530

    int

---
+++ integer.js
@@ -0,0 +1,11 @@
+
+//To find the average of a list of integers ;
+
+//literally i can't understand this question sorry ....
+
+
+const arr = [0, 10, 20, 30];
+
+const result = arr.reduce((acc,value)=> acc+value)/arr.length;
+
+console.log("result:",result);
