504e2814af7c6b2d838c3b3f817a0d289309e0e1
Pavan <101943293+Pavana004@users.noreply.github.com>
2022-11-19 10:41:28 +0530

    complete

--- router.js
+++ router.js
@@ -1,46 +1,28 @@
 const router = require("express").Router();
 const userdata = require("./modal");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const nodemailer = require("nodemailer");
-const validation = require("./verificationToken")
-require("dotenv").config();
 
 
-const key = "userinfoSecretId";
 
-//create mail host 
 
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    secure: true, 
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD,
-    },
-    tls: {
-      rejectUnauthorized: false
-    }
-})
+//post
 
-//signup
-
-router.post("/register",async (req,res)=>{
+router.post("/registration", async (req, res) => {
 
     try {
-        let emailExit = await userdata.findOne({ email: req.body.email })
-        if (emailExit) {
-            return res.status(400).json("Email already taken");
+        const Exituser = await userdata.findOne({ mobilenumber: req.body.mobilenumber })
+        if (Exituser) {
+            return res.status(400).json("Mobilenumber already taken");
         }
-        let psHash = await bcrypt.hash(req.body.password, 10);
         const data = new userdata({
-            name: req.body.name,
-            email: req.body.email,
-            password: psHash,
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            mobilenumber: req.body.mobilenumber,
+            address: req.body.address,
+            department: req.body.department,
         });
-        let result = await data.save();
-        res.json(result);
-       
+        const result = await data.save();
+        res.status(200).json(result);
+
 
 
     } catch (error) {
@@ -50,81 +32,62 @@ router.post("/register",async (req,res)=>{
 
 });
 
-//login
 
-router.post("/login",async(req,res)=>{
+//patch
+
+
+router.put("/update/:id", async (req, res) => {
     try {
-        
-        let userExit = await userdata.findOne({ email: req.body.email })
-        if (!userExit) {
-            return res.status(400).json("Your Email Wrong");
-        }
-        let passwordValidation = await bcrypt.compare(req.body.password, userExit.password);
-        if (!passwordValidation) {
-            return res.status(400).json("Your Password Wrong");
-        }
-        const userToken = jwt.sign({ email: userExit.email, name: userExit.name,id:userExit._id },key,{expiresIn:"1d"});
-        res.header("auth",userToken).send(userToken);
+
+        const finduser = await userdata.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+        res.status(200).json(finduser);
 
 
     } catch (error) {
+
         res.status(500).send({ message: "INPUT ERROR" });
+
     }
+})
+
+//get
+
+
+router.get("/list", async (req, res) => {
+    try {
+
+        const userlist = await userdata.find();
+        res.status(200).json(userlist);
+
+
+
+    } catch (error) {
+
+        res.status(500).send({ message: "INPUT ERROR" });
+
+    }
+
 });
 
 
-router.post("/forgotpassword",async (req,res)=>{
+
+// delete 
+
+
+router.delete("/delete/:id",async(req,res)=>{
     try {
+
+       await userdata.findByIdAndDelete(req.params.id);
+       res.status(200).json("User Deleted Successfully");
+
         
-     const exituser = await userdata.findOne({email:req.body.email});
-     if(!exituser){
-        return res.json("user not exists");
-     }
-     
-     const secret = key+exituser.password;
-     const userToken = jwt.sign({ email: exituser.email,id:exituser._id },secret,{expiresIn:"5m"});
-
-     transporter.sendMail({
-        to: exituser.email,
-        from: process.env.EMAIL,
-        subject: "verification mail",
-        text:`This link http://localhost:8000/forgotpassword/${userToken}`
-     })
-     
-     res.status(200).json("password send");
-       
-     
-
     } catch (error) {
         res.status(500).send({ message: "INPUT ERROR" });
-        
     }
 })
 
 
-//sendmail
 
-router.post("/mailsend",validation, async(req,res)=>{
-
-     try {
-         
-        const userInfo = await userdata.findOne({email:req.body.email});
-        transporter.sendMail({
-            to: userInfo.email,
-            from: process.env.EMAIL,
-            subject: "verification mail",
-            html:`<p>user verification mail</p>`
-        })
-        res.send("MAIL SEND....!");
-     } catch (error) {
-         res.status(500).send({ message: "INPUT ERROR" });
-     }
-
-
-})
-
-
-
-module.exports= router;
+module.exports = router;
 
 
